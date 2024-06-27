60519d5e10ab4b6787a48791cc151bd2b9972388
shahbazze <sirfstudy012@gmail.com>
2024-06-27 16:52:50 +0500

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,32 @@
+import Header from "./Header/Header";
+import Select from "./Select";
+import Footer from "./Footer/Footer";
+import Container from "./Container/Container";
+import Logo from "./Logo";
+import LogoutButton from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup"
+import Login  from "./Login";
+import PostForm from "./PostForm/PostForm"
+import PostCard from "./PostCard"
+import Button from "./Button";
+import AuthLayout from "./AuthLayout"
+import Input from "./Input";
+import LogoutBtn from "../components/Header/LogoutBtn"
+export {
+    Header,
+    LogoutBtn,
+    Footer,
+    Container,
+    Logo,
+    LogoutButton,
+    RTE,
+    Signup,
+    Login,
+    PostForm,
+    PostCard,
+    Button,
+    AuthLayout,
+    Select,
+    Input,
+}
